94c6d345523600f774ea14796f87efeb1f36b355
André Moreira <andre.moreira.9@hotmail.com>
2021-11-08 18:55:24 +0000

    i dont even know

--- practical/p02/ex2.23.hs
+++ practical/p02/ex2.23.hs
@@ -3,5 +3,15 @@ addPoly a b = reverse (take n (zipWith (+) (reverse a ++ repeat 0) (reverse b ++
   where
     n = maximum [length a, length b]
 
--- multPoly :: [Int] -> [Int] -> [Int]
--- multPoly a b = zip
+-- -- multPoly :: [Int] -> [Int] -> [Int]
+-- -- multPoly a b = zip
+-- [1, 3, -1]
+-- [3, 0, 2]
+
+-- [[(1, 3)], [(1, 0), (3, 3)], [(1, 2), (3, 0), (-1, 3)], [(3, 2), (-1, 0)], [(-1, 2)]]
+
+res a1 a2 = map (\(a, n) -> a ++ (take n (repeat 0))) (zip res (reverse [0 .. (length res - 1)]))
+  where
+    res = [[x * y | y <- a2] | x <- a1]
+
+final a1 a2 = foldr addPoly [] (res a1 a2)
